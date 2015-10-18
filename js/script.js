169fa8a580b0305e2a3f2ab4fc2a376c4b12d2d2
Keith Yong <terda12@gmail.com>
2015-10-18 19:08:38 -0400

    Working feature

--- js/script.js
+++ js/script.js
@@ -6,16 +6,24 @@
  * +----------------------+
  */
 
-/* Global state variables */
-var isRunning = false;
-var isWork = true;
+
+var config = {
+    initial_time_mode: "2510",
+    status_work_text: "WORK",
+    status_break_text: "BREAK"
+};
+
+var state = {
+    "is_running": false,
+    "is_work": true,
+    "sound_enabled": true,
+    "time_mode": "4515"
+};
+
 
 /* Global options variables */
-var soundEnabled = true;
-var pomodoro_time_seconds = 1500;
-var break_time_seconds = 600;
-var status_work_text = "WORK";
-var status_break_text = "BREAK";
+var pomodoro_time_seconds = 0, break_time_seconds = 0;
+updateTimeMode(config.initial_time_mode);
 
 /* Audio file for timer done sound */
 var timer_done_sound = new Audio("sounds/KitchenTimerSound.mp3");
@@ -25,6 +33,8 @@ var work_bg = document.getElementById("work_background");
 var break_bg = document.getElementById("break_background");
 var statusLine = document.getElementById("status");
 var sound_toggle_icon = document.getElementById("sound_toggle_icon");
+var time_mode_switch = document.getElementById("time_mode_switch");
+var onoffswitch_time_mode = $("#onoffswitch_time_mode");
 
 /* Initial setup */
 setUpWorkTimer();
@@ -32,8 +42,8 @@ document.title = "Pomodoro Timer";
 
 /* onClick events */
 $("#pausePlayButton").click(function() {
-    if (isRunning === false) {
-        resumeTimer(isWork);
+    if (state.is_running === false) {
+        resumeTimer(state.is_work);
     } else {
         pauseTimer();
     }
@@ -47,18 +57,35 @@ $("#breakTimerButton").click(function() {
     setUpBreakTimer();
 });
 
+$(time_mode_switch).click(function() {
+    timeModeToggleClick();
+});
+
 /* +--- soundToggleClick ------------------------+
  * | Called when user clicks sound toggle icon.  |
  * | Toggles sound on/off. Changes icon image.   |
  * +---------------------------------------------+ */
 function soundToggleClick() {
-    if (soundEnabled === true) {
+    if (state.sound_enabled === true) {
         sound_toggle_icon.src="images/soundoff.svg";
-        soundEnabled = false;
+        state.sound_enabled = false;
     } else {
         sound_toggle_icon.src="images/soundon.svg";
-        soundEnabled = true;
+        state.sound_enabled = true;
     }
+}
+
+/* +--- timeModeToggleClick() --------------------+
+ * | Called when user timemode switch.           |
+ * | Toggles time_mode 25/45.                    |
+ * +---------------------------------------------+ */
+function timeModeToggleClick() {
+    if (time_mode_switch.checked === true) {
+        state.time_mode = "4515";
+    } else {
+        state.time_mode = "2510";
+    }
+    setUpWorkTimer();
 }
 
 /* +--- setUpBreakTimer -------------------------+
@@ -67,11 +94,14 @@ function soundToggleClick() {
  * | Sets up status line, and button text.       |
  * +---------------------------------------------+ */
 function setUpBreakTimer() {
-    statusLine.innerHTML = status_break_text;
+    if (onoffswitch_time_mode.hasClass('work_mode'))
+        onoffswitch_time_mode.toggleClass('work_mode break_mode');
+
+    statusLine.innerHTML = config.status_break_text;
     pausePlayButton.innerHTML = "Start Break";
     transitionToBreakBackground();
-    isRunning = false;
-    isWork = false;
+    state.is_running = false;
+    state.is_work = false;
 
     $("#timer").createTimer({
         autostart: false,
@@ -86,18 +116,29 @@ function setUpBreakTimer() {
  * | Sets up status line, and button text.       |
  * +---------------------------------------------+ */
 function setUpWorkTimer() {
-    statusLine.innerHTML = status_work_text;
+    if (onoffswitch_time_mode.hasClass('break_mode'))
+        onoffswitch_time_mode.toggleClass('break_mode work_mode');
+
+    if (state.time_mode === "2510") {
+        pomodoro_time_seconds = 1500;
+        break_time_seconds = 600;
+    } else if (state.time_mode === "4515") {
+        pomodoro_time_seconds = 2700;
+        break_time_seconds = 900;
+    } else {
+        console.log("Invalid config, time_mode must be 2510 or 4515")
+    }
+
+    statusLine.innerHTML = config.status_work_text;
     pausePlayButton.innerHTML = "Start Work";
     transitionToWorkBackground();
-    isRunning = false;
-    isWork = true;
+    state.is_running = false;
+    state.is_work = true;
 
     $("#timer").createTimer({
         autostart: false,
         time_in_seconds: pomodoro_time_seconds,
-        tick: onTick(),
         buzzer: finishedWork
-       
     });
 }
 
@@ -126,11 +167,11 @@ var finishedBreak = function(timer) {
 }
 
 /* +--- pauseTimer ------------------------------+
- * | Pauses the timer. Sets isRunning to false.  |
+ * | Pauses the timer. Sets state.is_running to false.  |
  * | Sets the button text to be "Resume".        |
  * +---------------------------------------------+ */
 function pauseTimer() {
-    isRunning = false;
+    state.is_running = false;
     pausePlayButton.innerHTML = "Resume";
     $("#timer").pauseTimer();
 }
@@ -139,12 +180,12 @@ function pauseTimer() {
  * | Resumes the timer.                          |
  * | Sets the button text to be "Pause".         |
  * +---------------------------------------------+ */
-function resumeTimer(isWork) {
-    isRunning = true;
+function resumeTimer(is_work) {
+    state.is_running = true;
     pausePlayButton.innerHTML = "Pause";
 
     var buzzerCallbackFunc;
-    if (isWork === true)
+    if (state.is_work === true)
         buzzerCallbackFunc = finishedWork;
     else
         buzzerCallbackFunc = finishedBreak;
@@ -172,10 +213,24 @@ function transitionToWorkBackground() {
  * | Plays buzzer sound, only if not muted.     |
  * +--------------------------------------------+ */
 function playTimerDoneSound() {
-    if (soundEnabled === true)
+    if (state.sound_enabled === true)
         timer_done_sound.play();
 }
 
-function onTick(a, b, c) {
-    console.log(c);
+/* +--- updateTimeMode -------------------------+
+ * | Updates the state.time_mode variable.      |
+ * +--------------------------------------------+ */
+function updateTimeMode(mode) {
+    if (mode === "2510") {
+        state.time_mode = mode;
+        pomodoro_time_seconds = 1500;
+        break_time_seconds = 600;
+    } else if (mode === "4515") {
+        state.time_mode = mode;
+        pomodoro_time_seconds = 2700;
+        break_time_seconds = 900;
+    } else {
+        console.log("Invalid config, time_mode must be 2515 or 4515")
+        return;
+    }
 }
